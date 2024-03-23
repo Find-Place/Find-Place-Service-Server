0b49314c0a31d914a3422bb7ee934c444eb7e00b
sion <sion011218@naver.com>
2024-03-23 21:10:11 +0900

    Modification response type

--- controller/controller.js
+++ controller/controller.js
@@ -4,6 +4,7 @@ const request = require('request');
 const FormData = require('form-data');
 const axios = require('axios');
 const fs = require('fs');
+const json_id = 1;
 
 async function processImage(req, res, next) {
   if (!req.file) {
@@ -29,16 +30,21 @@ async function processImage(req, res, next) {
   })
     .then(response => {
       console.log('응답 받음:');
-      console.log(response.data)
+      const location_info = response.data.data
+      console.log(location_info);
+      res.status(200).json({
+        id: json_id,
+        locaiton: location_info
+      });
     })
     .catch(error => {
       console.error('에러 발생:');
+      res.status(404).json({
+        value: "error"
+      });
     });
 
-  res.status(200).json({
-    id: uploadad_photo.id,
-    filePath: uploadad_photo.url
-  });
+
 }
 
 // start dev!
@@ -46,3 +52,5 @@ async function processImage(req, res, next) {
 module.exports = {
   processImage: processImage
 }
+
+
